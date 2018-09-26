54d14e5f2448186664e62e7352b69cf3edba85de
Gavin Ceballos <gavinceb.61448@redlandsschools.net>
2018-09-26 15:25:21 +0000

    egg

---
+++ main.py
@@ -0,0 +1,25 @@
+#Functions
+
+def firstChoice():
+    print("The Value Grocery store lies at the end of a forest with a lava floor.")
+    print("The Premium Grocery is accessible through a large waterside outside your house.")
+    print("Both of these grocery stores have the eggs for your omellete.")
+    choice = input("Which grocery store do you want to get the eggs from? (Value or Premium):  ")
+    return choice
+
+def lavaMonkey():
+    print("While staying away from the lava isn't particularly difficult, a lava monkey stands in the way between you and the eggs you seek.")
+    print("Being the fantastic chef you are, you always carry a set of utensils with you at all times.")
+    print("In order to get past the monkey, you are condidering using either the Spoon or the Fork as a weapon.")
+    weapon = input("Spoon or Fork? ")
+    return weapon
+    
+def 
+    
+    
+    
+    
+    
+    
+#Remeber to add beginning of story
+store = firstChoice()
