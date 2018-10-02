2ea0fdc417c5e611124feb4d00478f8bcf659a70
Gavin Ceballos <gavinceb.61448@redlandsschools.net>
2018-10-02 16:16:45 +0000

    Added more color values, consistancy

--- main.py
+++ main.py
@@ -21,61 +21,33 @@ def firstChoice():
     elif choice == "Premium" or choice == "premium":    #See "Premium Route"
         waterSlide()
     else:   #The Catch
-        print("You look around, but do not see a store by that name.")
+        print(Fore.MAGENTA, Back.WHITE + "You look around, but do not see a store by that name." + Style.RESET_ALL)
+        time.sleep(1)
         firstChoice()
 """
 Value Market Route
 """
 def lavaMonkey():
-    print("While staying away from the lava isn't particularly difficult, a " + Back.RED + Fore.BLACK + "lava monkey" + Style.RESET_ALL + " stands in the way between you and the eggs you seek.")
+    print("While staying away from the lava isn't particularly difficult, a " + Fore.BLACK + Back.RED + "lava monkey" + Style.RESET_ALL + " stands in the way between you and the eggs you seek.")
     print("Being the fantastic chef you are, you always carry a set of utensils with you at all times.")
-    print("In order to get past the monkey, you consider using either the Spoon or the Fork as a weapon.")
-    weapon = input("Spoon or Fork? ")
+    print("In order to get past the monkey, you consider using either the " + Fore.CYAN +  "Spoon" + Style.RESET_ALL + " or the " + Fore.RED + "Fork" + Style.RESET_ALL + " as a weapon.")
+    weapon = input(Fore.GREEN + "Spoon" + Style.RESET_ALL +" or " + Fore.RED + "Fork?" + " " + Style.RESET_ALL)
     if weapon == "Spoon" or weapon == "spoon": #Lose choice
         loseSpoon()
     elif weapon == "Fork" or weapon == "fork": #Win choice
         fightGrandma()
     else:
-        print("That is not a Fork or a Spoon.")
+        print(Fore.MAGENTA, Back.WHITE + "That is not a Fork or a Spoon." + Style.RESET_ALL)
+        time.sleep(1)
         lavaMonkey()
 
 #Lose Spoon
 def loseSpoon():
     print("The monkey deftly slaps the spoon out of your hand.")
     print("You are sad, but the spoon wouldn't have done much anyways.")
-    print("Weaponless and eggless, you return home hungry.")
-    print("You Lose!")
-    gameExit = True
-    return gameExit
-#Win Spoon
-def fightGrandma():
-    print("The lava monkey does not like being poked, and he returns to his treetop home opening the way forward.")
-    print("You make it inside the Value Grocery store, but by the time you make it to the eggs, a normal grocery store grandma has taken the last dozen!")
-    print("You REALLY want that omellete, but would you fight a grandma for it?")
-    fight = input("(Yes or No): ")
-    if fight == "Yes" or fight == "yes": #Win Choice
-        valueEggs()
-    elif fight == "No" or fight == "no": #Lose Choice
-        loseGrandma()
-    else:
-        print("Please answer either Yes or No or you cannot go on.")
-        fightGrandma()
-
-#Lose Grandma 
-def loseGrandma():
-    print("Although it is tempting to snatch the eggs away from grandma, you decide to let her keep the eggs.")
-    print("The Grandma notices your sad face and gives you a lolipop for letting her keep the eggs.")
-    print("With a lolipop in your hand, you return to your eggsless home.")
-    print("You Lose!")
-    gameExit = True
-    return gameExit
-
-#Win Grandma
-def valueEggs():
-    print("The grandma puts up a tough fight, but you manage to wrangle the eggs away from her.")
-    print("You (Gordon Ramsay) have successfully aquired eggs from the Value Market and have made yourself a delicious omellete!")
-    print("Since you fought a grandma for eggs in a grocery store, I'm unsure you can call this a moral victory, but you won either way.")
-    exit = input("Do you wish to exit the game? (Yes or No): ")
+    print("Weaponless and" + Fore.YELLOW, Style.BRIGHT + "eggless" + Style.RESET_ALL + ", you return home hungry.")
+    print(Fore.RED, Back.WHITE, Style.BRIGHT + "You Lose!" + Style.RESET_ALL)
+    exit = input("Do you wish to exit the game? (" + Fore.GREEN + "Yes" + Style.RESET_ALL + " or " + Fore.RED + "No" + Style.RESET_ALL + "): ")
     if exit == "Yes" or exit == "yes":
         gameExit = True
         return gameExit
@@ -83,7 +55,60 @@ def valueEggs():
         print ("Fine, then you can sit here.")
         input()
     else:    
-        print("I'm going to assume that means you want to leave.")
+        print(Fore.MAGENTA, Back.WHITE + "I'm going to assume that means you want to leave." + Style.RESET_ALL)
+        time.sleep(3)
+        gameExit = True
+        return gameExit
+#Win Spoon
+def fightGrandma():
+    print("The lava monkey does not like being poked, and he returns to his treetop home opening the way forward.")
+    print("You make it inside the Value Grocery store, but by the time you make it to the eggs, a normal grocery store grandma has taken the " + Fore.GREEN + "last" + Style.RESET_ALL + " dozen!")
+    print("You " + Fore.RED + "REALLY" + Style.RESET_ALL + " want that omellete, but would you fight a grandma for it?")
+    fight = input("(" + Fore.GREEN + "Yes" + Style.RESET_ALL + " or " + Fore.RED + "No" + Style.RESET_ALL + "): ")
+    if fight == "Yes" or fight == "yes": #Win Choice
+        valueEggs()
+    elif fight == "No" or fight == "no": #Lose Choice
+        loseGrandma()
+    else:
+        print(Fore.MAGENTA, Back.WHITE + "Please answer either Yes or No or you cannot go on." + Style.RESET_ALL)
+        time.sleep(1)
+        fightGrandma()
+
+#Lose Grandma 
+def loseGrandma():
+    print("Although it is tempting to snatch the eggs away from grandma, you decide to let her keep the eggs.")
+    print("The Grandma notices your sad face and gives you a lolipop for letting her keep the eggs.")
+    print("With a lolipop in your hand, you return to your eggsless home.")
+    print(Fore.RED, Back.WHITE, Style.BRIGHT + "You Lose!" + Style.RESET_ALL)
+    exit = input("Do you wish to exit the game? (" + Fore.GREEN + "Yes" + Style.RESET_ALL + " or " + Fore.RED + "No" + Style.RESET_ALL + "): ")
+    if exit == "Yes" or exit == "yes":
+        gameExit = True
+        return gameExit
+    elif exit == "No" or exit == "no": #HOLD
+        print ("Fine, then you can sit here.")
+        input()
+    else:    
+        print(Fore.MAGENTA, Back.WHITE + "I'm going to assume that means you want to leave." + Style.RESET_ALL)
+        time.sleep(3)
+        gameExit = True
+        return gameExit
+
+#Win Grandma
+def valueEggs():
+    print("The grandma puts up a tough fight, but you manage to wrangle the eggs away from her.")
+    print("You " + Fore.RED +  "(Gordon Ramsay)" + Style.RESET_ALL + " have successfully aquired eggs from the Value Market and have made yourself a delicious omellete!")
+    print("Since you fought a grandma for eggs in a grocery store, I'm unsure you can call this a moral victory, but you won either way.")
+    print(Fore.GREEN, Back.WHITE, Style.BRIGHT + "You Win!" + Style.RESET_ALL)
+    exit = input("Do you wish to exit the game? (" + Fore.GREEN + "Yes" + Style.RESET_ALL + " or " + Fore.RED + "No" + Style.RESET_ALL + "): ")
+    if exit == "Yes" or exit == "yes":
+        gameExit = True
+        return gameExit
+    elif exit == "No" or exit == "no": #HOLD
+        print ("Fine, then you can sit here.")
+        input()
+    else:    
+        print(Fore.MAGENTA, Back.WHITE + "I'm going to assume that means you want to leave." + Style.RESET_ALL)
+        time.sleep(3)
         gameExit = True
         return gameExit
 
@@ -94,27 +119,30 @@ def waterSlide():
     print("You approach the waterslide, expecting a quick journey, only to find that it is completely dry!")
     print("Being the fantastic chef you are, You are carrying a full stick of butter with you.")
     print("At this point you can either butter yourself up for the slide, or run down it barefoot (to avoid slipping).")
-    slide = input("Butter or Run? ")
+    slide = input(Fore.GREEN + "Butter" + Style.RESET_ALL + " or " + Fore.RED + "Run? " + Style.RESET_ALL)
     if slide == "Butter" or slide == "butter": #Win choice
         butter()
     elif slide == "Run" or slide == "run": #Lose Choice
         run()
     else:
-        print("Gordon's inability to decide will force him to run down the slide!")
-        run()
+        print(Fore.MAGENTA, Back.WHITE + "Gordon's inability to decide will get him nowhere!" + Style.RESET_ALL)
+        time.sleep(1)
+        waterSlide()
 
 def butter():
-    print("You quickly whip out your stick of fresh butter and begin to cover yourself in a thick layer of butter.")
+    print("You quickly whip out your stick of fresh " + Fore.YELLOW + "butter" + Style.RESET_ALL + "and begin to cover yourself in a thick layer of butter.")
     print("Now that you're all buttered up, you get on the slide and start cruising down to the market.")
     print("You finally arrive to the Premium Market and grab the glorious eggs, BUT you realize you don't have enough money!")
     print("Would you steal the eggs for your precious omellete?")
-    steal = input("Do you wish to steal the eggs? (Yes or No): ")
+    steal = input("Do you wish to steal the eggs? (" + Fore.GREEN + "Yes" + Style.RESET_ALL + " or " + Fore.RED + "No" + Style.RESET_ALL + "): ")
     if steal == "Yes" or steal == "yes": #Win Choice
         yesSteal()
     elif steal == "No" or steal == "no": #Lose Choice
         noSteal()
     else:
-        print("Please say either Yes or No if you want to go on.")
+        print(Fore.MAGENTA, Back.WHITE + "Please say either Yes or No if you want to go on." + Style.RESET_ALL)
+        time.sleep(1)
+        butter()
 
 #Win Steal
 def yesSteal():
@@ -122,8 +150,8 @@ def yesSteal():
     print("As you walk closer and closer to the exit, you dash immediately out of the store after one of your eggs fell on the floor!")
     print("The security guard begins chasing after you, but you manage to get away from him and safely return home.")
     print("Now that you have the eggs, you make the most delicious omellete ever and enjoy your meal.")
-    print("You Win!")
-    exit = input("Do you wish to exit the game? (Yes or No): ")
+    print(Fore.GREEN, Back.WHITE, Style.BRIGHT + "You Win!" + Style.RESET_ALL)
+    exit = input("Do you wish to exit the game? (" + Fore.GREEN + "Yes" + Style.RESET_ALL + " or " + Fore.RED + "No" + Style.RESET_ALL + "): ")
     if exit == "Yes" or exit == "yes":
         gameExit = True
         return gameExit
@@ -131,7 +159,8 @@ def yesSteal():
         print ("Fine, then you can sit here.")
         input()
     else:    
-        print("I'm going to assume that means you want to leave.")
+        print(Fore.MAGENTA, Back.WHITE + "I'm going to assume that means you want to leave." + Style.RESET_ALL)
+        time.sleep(3)
         gameExit = True
         return gameExit
     
@@ -139,18 +168,38 @@ def yesSteal():
 def noSteal():
     print("You decide not to steal the eggs because it's the wrong thing to do and you don't want to go to jail.")
     print("You return to your eggless home and decide to just cook some omellete flavored cup noodles instead.")
-    print("You Lose!")
-    gameExit = True
-    return gameExit
+    print(Fore.RED, Back.WHITE, Style.BRIGHT + "You Lose!" + Style.RESET_ALL)
+    exit = input("Do you wish to exit the game? (" + Fore.GREEN + "Yes" + Style.RESET_ALL + " or " + Fore.RED + "No" + Style.RESET_ALL + "): ")
+    if exit == "Yes" or exit == "yes":
+        gameExit = True
+        return gameExit
+    elif exit == "No" or exit == "no": #HOLD
+        print ("Fine, then you can sit here.")
+        input()
+    else:    
+        print(Fore.MAGENTA, Back.WHITE + "I'm going to assume that means you want to leave." + Style.RESET_ALL)
+        time.sleep(3)
+        gameExit = True
+        return gameExit
     
 #Lose Run
 def run():
     print("You take off your shoes in order to run down the hot, burning, plastic slide.")
     print("As you run down the slide, you're in agonizing pain because you burned you feet.")
     print("Suddenly the slide begins to work properly again and a gush of water begins to flow down the slide!")
-    print("You slip and begin to enjoy the ride until the slide makes you miss Whole Foods and you are separated from your precious eggs.")
-    print("You Lose!")
-    gameExit = True
-    return gameExit
+    print("You slip and begin to enjoy the ride until the slide makes you miss the " + Fore.CYAN +  "Premium Grocery" + Style.RESET_ALL + " and you are separated from your precious " + Fore.YELLOW + "eggs." + Style.RESET_ALL)
+    print(Fore.RED, Back.WHITE, Style.BRIGHT + "You Lose!" + Style.RESET_ALL)
+    exit = input("Do you wish to exit the game? (" + Fore.GREEN + "Yes" + Style.RESET_ALL + " or " + Fore.RED + "No" + Style.RESET_ALL + "): ")
+    if exit == "Yes" or exit == "yes":
+        gameExit = True
+        return gameExit
+    elif exit == "No" or exit == "no": #HOLD
+        print ("Fine, then you can sit here.")
+        input()
+    else:    
+        print(Fore.MAGENTA, Back.WHITE + "I'm going to assume that means you want to leave." + Style.RESET_ALL)
+        time.sleep(3)
+        gameExit = True
+        return gameExit
     
 firstChoice()
--- test.py
+++ test.py
@@ -1,28 +1,4 @@
-lives = 5
+from colorama import Style, Fore, Back
 
-gameExit = False
-
-while (gameExit == True):
-    break
-
-def firstChoice(lives):
-    print("The Value Grocery store lies at the end of a forest with a lava floor.")
-    print("The Premium Grocery store is accessible through a large waterside outside your house.")
-    print("Both of these grocery stores have the eggs for your omellete.")
-    choice = input("Which grocery store do you want to get the eggs from? (Value or Premium): ")
-    if lives == 0:
-        print("Clearly confused, Gordon heads back inside to get more sleep.")
-        gameExit = True
-        return gameExit
-    
-    if choice == "Value" or choice == "value":  #See "Value Route"
-        print("s")
-    elif choice == "Premium" or choice == "premium":    #See "Premium Route"
-        print("l")
-    else:   #The Catch
-        lives = lives - 1
-        print("You look around, but do not see a store by that name.")
-        firstChoice(lives)
-        
-firstChoice(lives)
+print(Fore.GREEN, Back.WHITE, Style.BRIGHT + "You Win!" + Style.RESET_ALL)
 
